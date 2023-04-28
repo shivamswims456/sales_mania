4a9acbf918a30e7882f3aee6adde194b1af27f4c
Shivam Prasad <shivamswims456@gmail.com>
2023-04-28 15:49:14 +0530

    sketch_3

---
+++ sketch/sketch_zoho_inv/3.py
@@ -0,0 +1,66 @@
+inv = ["invoice_id","invoice_number","date","date_formatted","due_date","due_date_formatted","offline_created_date_with_time","customer_id","customer_name","customer_custom_fields","customer_custom_field_hash","email","currency_id","invoice_source","invoice_source_formatted","currency_code","currency_symbol","currency_name_formatted","status","status_formatted","custom_fields","custom_field_hash","recurring_invoice_id","place_of_supply","place_of_supply_formatted","payment_terms","payment_terms_label","payment_reminder_enabled","payment_made","payment_made_formatted","crm_owner_id","crm_custom_reference_id","zcrm_potential_id","zcrm_potential_name","next_retry_date","next_retry_date_formatted","next_retry_number","reference_number","lock_details","line_items","credits","journal_credits","exchange_rate","is_autobill_enabled","inprocess_transaction_present","allow_partial_payments","price_precision","sub_total","sub_total_formatted","tax_total","tax_total_formatted","discount_total","discount_total_formatted","discount_percent","discount","discount_applied_on_amount","discount_type","is_discount_before_tax","adjustment","adjustment_formatted","adjustment_description","shipping_charge_tax_id","shipping_charge_tax_name","shipping_charge_tax_type","shipping_charge_tax_percentage","shipping_charge_tax_exemption_id","shipping_charge_tax_exemption_code","shipping_charge_sac_code","shipping_charge_tax","bcy_shipping_charge_tax","shipping_charge_exclusive_of_tax","shipping_charge_inclusive_of_tax","shipping_charge_tax_formatted","shipping_charge_exclusive_of_tax_formatted","shipping_charge_inclusive_of_tax_formatted","shipping_charge","shipping_charge_formatted","bcy_shipping_charge","bcy_adjustment","bcy_sub_total","bcy_discount_total","bcy_tax_total","bcy_total","is_reverse_charge_applied","total","total_formatted","balance","balance_formatted","write_off_amount","write_off_amount_formatted","roundoff_value","roundoff_value_formatted","transaction_rounding_type","reference_invoice_type","is_inclusive_tax","sub_total_inclusive_of_tax","sub_total_inclusive_of_tax_formatted","tax_specification","gst_no","gst_treatment","gst_treatment_formatted","tax_reg_no","contact_category","tax_treatment","tax_treatment_formatted","tax_rounding","taxes","filed_in_vat_return_id","filed_in_vat_return_name","filed_in_vat_return_type","gst_return_details","reverse_charge_tax_total","reverse_charge_tax_total_formatted","can_send_invoice_sms","payment_expected_date","payment_expected_date_formatted","payment_discount","payment_discount_formatted","stop_reminder_until_payment_expected_date","last_payment_date","last_payment_date_formatted","autobill_status","autobill_status_formatted","can_show_force_pay","ach_supported","ach_payment_initiated","payment_options","payments","reader_offline_payment_initiated","is_square_transaction","contact_persons","contact_persons_associated","attachment_name","documents","computation_type","debit_notes","deliverychallans","bills","ewaybills","dispatch_from_address","is_eway_bill_required","can_generate_ewaybill_using_irn","mark_as_shipment_status","is_standalone_invoice","merchant_id","merchant_name","merchant_gst_no","ecomm_operator_id","ecomm_operator_name","ecomm_operator_gst_no","salesorder_id","salesorder_number","salesorders","shipping_bills","unbilled_expenses_count","unbilled_bill_items_count","warn_convert_to_open","warn_create_creditnotes","contact_persons_details","comments","salesperson_id","salesperson_name","is_emailed","reminders_sent","last_reminder_sent_date","last_reminder_sent_date_formatted","next_reminder_date_formatted","is_portal_enabled","is_viewed_by_client","client_viewed_time","client_viewed_time_formatted","submitter_id","approver_id","submitted_date","submitted_date_formatted","submitted_by","submitted_by_name","submitted_by_email","submitted_by_photo_url","template_id","template_name","template_type","template_type_formatted","notes","terms","billing_address","shipping_address","subject_content","can_send_in_mail","created_time","last_modified_time","created_date","created_date_formatted","created_by_id","last_modified_by_id","page_width","page_height","orientation","is_backorder","sales_channel","is_pre_gst","type_formatted","type","color_code","current_sub_status_id","current_sub_status","current_sub_status_formatted","sub_statuses","reason_for_debit_note","reason_for_debit_note_formatted","estimate_id","is_client_review_settings_enabled","is_taxable","unused_credits_receivable_amount","unused_credits_receivable_amount_formatted","unused_retainer_payments","unused_retainer_payments_formatted","credits_applied","credits_applied_formatted","tax_amount_withheld","tax_amount_withheld_formatted","is_cloneable","can_send_in_mail_state","schedule_time","schedule_time_formatted","no_of_copies","show_no_of_copies","auto_reminders_configured","customer_default_billing_address","can_send_comment_notification","reference_invoice","includes_package_tracking_info","approvers_list"]
+
+bills = ["bill_id","purchaseorder_ids","vendor_id","vendor_name","source","source_of_supply","source_of_supply_formatted","destination_of_supply","destination_of_supply_formatted","gst_no","reference_invoice_type","contact_category","gst_treatment","gst_treatment_formatted","tax_treatment","tax_treatment_formatted","gst_return_details","invoice_conversion_type","unused_credits_payable_amount","unused_credits_payable_amount_formatted","status","status_formatted","color_code","current_sub_status_id","current_sub_status","current_sub_status_formatted","sub_statuses","bill_number","date","date_formatted","is_pre_gst","due_date","due_date_formatted","discount_setting","is_tds_amount_in_percent","tds_percent_formatted","tds_percent","tds_amount_formatted","tds_amount","tax_account_id","payment_terms","payment_terms_label","payment_expected_date","payment_expected_date_formatted","reference_number","recurring_bill_id","due_by_days","due_in_days","currency_id","currency_code","currency_symbol","currency_name_formatted","documents","price_precision","exchange_rate","custom_fields","custom_field_hash","is_viewed_by_client","client_viewed_time","client_viewed_time_formatted","is_tds_applied","is_item_level_tax_calc","is_inclusive_tax","tax_rounding","filed_in_vat_return_id","filed_in_vat_return_name","filed_in_vat_return_type","is_reverse_charge_applied","is_uber_bill","is_tally_bill","mark_as_received_status","is_standalone_bill","track_discount_in_account","line_items","submitted_date","submitted_date_formatted","submitted_by","submitted_by_name","submitted_by_email","submitted_by_photo_url","submitter_id","approver_id","adjustment","adjustment_formatted","adjustment_description","discount_amount_formatted","discount_amount","discount","discount_applied_on_amount_formatted","discount_applied_on_amount","is_discount_before_tax","discount_account_id","discount_account_name","discount_type","sub_total","sub_total_formatted","sub_total_inclusive_of_tax","sub_total_inclusive_of_tax_formatted","tax_total","tax_total_formatted","total","total_formatted","payment_made","payment_made_formatted","vendor_credits_applied","vendor_credits_applied_formatted","is_line_item_invoiced","purchaseorders","taxes","tax_override","balance","balance_formatted","billing_address","is_portal_enabled","payments","vendor_credits","journal_credits","comments","created_time","created_by_id","last_modified_id","last_modified_time","warn_create_vendor_credits","reference_id","notes","terms","attachment_name","open_purchaseorders_count","un_billed_items","template_id","template_name","page_width","page_height","orientation","template_type","template_type_formatted","invoices","is_approval_required","can_create_bill_of_entry","allocated_landed_costs","unallocated_landed_costs","entity_type","credit_notes","reference_bill_id","can_send_in_mail","approvers_list"]
+
+from pprint import pprint
+
+pprint(set(inv) - set(bills))
+
+
+""" 
+
+    {'allocated_landed_costs',
+ 'bill_id',
+ 'bill_number',
+ 'can_create_bill_of_entry',       
+ 'credit_notes',
+ 'destination_of_supply',
+ 'destination_of_supply_formatted',
+ 'discount_account_id',
+ 'discount_account_name',
+ 'discount_amount',
+ 'discount_amount_formatted',      
+ 'discount_applied_on_amount_formatted',
+ 'discount_setting',
+ 'due_by_days',
+ 'due_in_days',
+ 'entity_type',
+ 'invoice_conversion_type',
+ 'invoices',
+ 'is_approval_required',
+ 'is_item_level_tax_calc',
+ 'is_line_item_invoiced',
+ 'is_standalone_bill',
+ 'is_tally_bill',
+ 'is_tds_amount_in_percent',
+ 'is_tds_applied',
+ 'is_uber_bill',
+ 'last_modified_id',
+ 'mark_as_received_status',
+ 'open_purchaseorders_count',
+ 'purchaseorder_ids',
+ 'purchaseorders',
+ 'recurring_bill_id',
+ 'reference_bill_id',
+ 'reference_id',
+ 'source',
+ 'source_of_supply',
+ 'source_of_supply_formatted',
+ 'tax_account_id',
+ 'tax_override',
+ 'tds_amount',
+ 'tds_amount_formatted',
+ 'tds_percent',
+ 'tds_percent_formatted',
+ 'track_discount_in_account',
+ 'un_billed_items',
+ 'unallocated_landed_costs',
+ 'unused_credits_payable_amount',
+ 'unused_credits_payable_amount_formatted',
+ 'vendor_credits',
+ 'vendor_credits_applied',
+ 'vendor_credits_applied_formatted',
+ 'vendor_id',
+ 'vendor_name',
+ 'warn_create_vendor_credits'}
+"""
